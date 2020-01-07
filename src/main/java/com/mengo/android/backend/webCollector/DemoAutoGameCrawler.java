852e4497da9f9eda8c060932fc02adb2cf439058
mengo <867740815@qq.com>
2020-01-07 21:11:29 +0800

    学习采集接口

---
+++ src/main/java/com/mengo/android/backend/webCollector/DemoAutoGameCrawler.java
@@ -0,0 +1,80 @@
+package com.mengo.android.backend.webCollector;
+
+import cn.edu.hfut.dmic.webcollector.model.CrawlDatums;
+import cn.edu.hfut.dmic.webcollector.model.Page;
+import cn.edu.hfut.dmic.webcollector.plugin.berkeley.BreadthCrawler;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+/**
+ * 继承广度爬虫（breadth crawler 是最常用的爬取器直以）
+ */
+public class DemoAutoGameCrawler extends BreadthCrawler {
+    public DemoAutoGameCrawler(String crawlPath, boolean autoParse) {
+        super(crawlPath, autoParse);
+        /**
+         *设置爬取的网站地址
+         * addSeed表示添加种子
+         * 种子链接会在爬虫启动之前加入到爬取信息中并标记为未抓取状态，这个过程称为注入
+         */
+        this.addSeed("https://www.2dfan.com/subjects/page/1");
+        /**
+         * 循环添加4个种子，其实就是分页，结果类似：
+         * https://www.2dfan.com/subjects/page/2
+         * https://www.2dfan.com/subjects/page/3
+         * https://www.2dfan.com/subjects/page/4
+         * https://www.2dfan.com/subjects/page/5
+         */
+        for (int pageIndex = 2; pageIndex <= 5; pageIndex++) {
+            String seedUrl = String.format("https://www.2dfan.com/subjects/page/%d", pageIndex);
+            this.addSeed(seedUrl);
+        }
+        /**
+         * addRegex 参数为一个url正则表达式，可以用于过滤不必抓取的链接，比如.js .jpg .css……等
+         * 也可以指定抓取某些规则的链接，
+         */
+
+        /**
+         * 设置线程数
+         */
+        setThreads(20);
+        getConf().setTopN(40);
+    }
+
+    /**
+     * 必须重写visit 方法，作用是：
+     * 在整个抓取过程中，只要抓到符合要求的页面，webCollector就会回调该方法，并传入一个包含了页面所有信息的page对象
+     *
+     * @param page
+     * @param crawlDatums
+     */
+    @Override
+    public void visit(Page page, CrawlDatums crawlDatums) {
+        String url = page.url();
+        /**
+         * 如果此页面地址 确实是要求爬取的网址，则进行取值
+         */
+        if (page.matchUrl("https://www.2dfan.com/subjects/page/^[1-5]$/")){
+            Elements elements = page.select("h4[class=media-heading]");
+            for(Element e:elements){
+                System.out.println(e.text());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        /**
+         * 构造器中进行数据初始化，这2个参数会传递给父类
+         * crawlPath:表示设置保存爬虫记录的文件夹
+         */
+        DemoAutoGameCrawler crawler = new DemoAutoGameCrawler("crawl",true);
+        try {
+            /**
+             * 启动爬虫，爬取的深度为4层
+             */
+            crawler.start(4);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
